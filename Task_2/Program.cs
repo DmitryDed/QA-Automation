e0fd941ad36da97d6eb7439b91ea773349377eec
Dmitry Abramov <abrannik25@gmail.com>
2021-04-17 22:00:37 +0300

    commit 1

---
+++ Task_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int kol = 1;
+            int max = 0;
+            Console.WriteLine("Введите строку: ");
+            string str = Console.ReadLine();
+            for (int i = 0; i < (str.Length - 1); i++)
+            {
+                if (str[i] != str[i + 1])
+                {
+                    kol++;
+                    max = kol;
+                }
+                else
+                {
+                    kol = 1;
+                }
+            }
+            Console.WriteLine(max);
+        }
+    }
+}
