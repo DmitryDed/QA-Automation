082ffbba6235a81d49f88e1374bb6f0c5a725bc5
Dmitry Abramov <abrannik25@gmail.com>
2021-06-11 00:27:39 +0300

    Task3

---
+++ Task_3/Task_3/Bus.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_3
+{
+    public class Bus : Vehicle
+    {
+        public int Seats { get; set; }
+
+        public Bus(double engine_disp, string transmission, int max_speed, int seats) : base(engine_disp, transmission, max_speed)
+        {
+            Seats = seats;
+        }
+
+        public override string GetFullInfo()
+        {
+            return (base.GetFullInfo() + $"Number of seats: {Seats}");
+        }
+    }
+}
---
+++ Task_3/Task_3/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_3
+{
+    class Car : Vehicle
+    {
+        public int Doors { get; set; }
+
+        public Car(double engine_disp, string transmission, int max_speed, int doors) : base    
+        {
+            Doors = doors;
+        }
+
+        public override string GetFullInfo()
+        {
+            return (base.GetFullInfo() + $"Number of doors: {Doors}");
+        }
+    }
+}
